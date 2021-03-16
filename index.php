40b201605eb0d7b38fe984cf59c6bd19d6cca161
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-03-16 18:18:43 +0100

    fatto tutto ma non mi stampa i dati dell'utente

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <?php
-
-
 class prodotti{
-    private $nome;
+    public $nome;
 
-    private $img;
+    public $img;
     
-    private $descrizioneProdotto;
+    public $descrizioneProdotto;
 
-    private $prezzo;
+    public $prezzo;
 
-    private $recensione;
+    public $recensione;
 
     public function __construct($nome, $img, $descrizioneProdotto, $prezzo, $recensione){
 
@@ -24,16 +22,16 @@ class prodotti{
 
 }
 
+class musical extends prodotti {
 
+}
+class shose extends prodotti {
 
-$chitarra = new prodotti ( 'fender', 'img', 'é un chitarra famosa usata da personaggi famosi', ' euro 2000', ' voto 5') ;
+}
+class glass extends prodotti {
 
+}
 
-echo $chitarra -> nome;
-echo $chitarra -> img;
-echo $chitarra -> descrizioneProdotto;
-echo $chitarra -> prezzo;
-echo $chitarra -> recensione;
 //parte utente
 
 class user {
@@ -50,21 +48,66 @@ class user {
 
     protected $fattura;
 
+    protected $carrello=[];
+
+
     public function __construc($nome,$cognome,$indirizzo,$telefono,$email,$fattura){
   
     $this ->nome =$nome;
     $this ->cognome =$cognome;
-    $this ->indirizzoFatturazione =$indirizzo;
+    $this ->indirizzo =$indirizzo;
     $this ->telefono =$telefono;
     $this ->email =$email;
     $this ->fattura =$fattura;
 
     }
+    //parte funzione di aquisto utente
+
+    public function addShops($prodotti){
+        $this->carrello[] = $prodotti;
+    }
+    public function getProducts() {
+        return $this->$prodotti;
+      }
   
   }
 
-  class creditcard{
-      
-  }
+  class creditCard{
+    protected $nomeAzienda;
 
+    protected $nomeUser;
+
+    protected $scadenza;
+  
+    protected $cv;
+
+    public function __construc($nomeAzienda,$nomeUser,$scadenza,$cv){
+    
+      $this ->nomeAzienda =$nomeAzienda;
+      $this ->nomeUser =$nomeUser;
+      $this ->scadenza =$scadenza;
+      $this ->cv =$cv;
+   
+  
+      }
+  }
+  $chitarra = new musical ( 'fender', 'img', 'é un chitarra famosa usata da personaggi famosi', ' euro 2000', ' voto 5') ;
+
+
+//   echo $chitarra -> nome;
+//   echo $chitarra -> img;
+//   echo $chitarra -> descrizioneProdotto;
+//   echo $chitarra -> prezzo;
+//   echo $chitarra -> recensione;
+
+  //parte di aquisto utente
+  $shopUser = new user('mario','rossi','via Colombo','340*****03','marioògmail.com','fat-12455-658/3');
+
+  $shopUser -> addShops($chitarra);
+
+  var_dump($shopUser);
+
+
+  
 ?>
+
